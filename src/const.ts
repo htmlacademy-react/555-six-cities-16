acb272c4d9918710053aa46ae1d2deab9597cb28
Etel Bogen <etel.bogen@gmail.com>
2024-08-24 06:16:00 +0300

    Перенесла константы в файл с константами

--- src/const.ts
+++ src/const.ts
@@ -36,3 +36,7 @@ export enum AuthorizationStatus {
   NoAuth = 'NO_AUTH',
   Unknown = 'UNKNOWN',
 }
+
+export const URL_MARKER_DEFAULT = './img/pin.svg';
+
+export const URL_MARKER_ACTIVE = './img/pin-active.svg';
